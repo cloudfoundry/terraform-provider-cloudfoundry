867627bcc62eaca6a5ad0f1fd0e3f7bc5ab40a8c
Debaditya <debadityaray00@gmail.com>
2024-09-23 11:45:53 +0200

    docs: harmonize import examples

--- examples/resources/cloudfoundry_service_credential_binding/import.sh
+++ examples/resources/cloudfoundry_service_credential_binding/import.sh
@@ -1,1 +1,3 @@
+# terraform import cloudfoundry_service_credential_binding.<resource_name> <service_credential_binding_guid>
+
 terraform import cloudfoundry_service_credential_binding.xsuaa_svc 68fea1b6-11b9-4737-ad79-74e49832533f
--- examples/resources/cloudfoundry_service_route_binding/import.sh
+++ examples/resources/cloudfoundry_service_route_binding/import.sh
@@ -1,1 +1,3 @@
+# terraform import cloudfoundry_service_route_binding.<resource_name> <service_route_binding_guid>
+
 terraform import cloudfoundry_service_route_binding.xsuaa_bi 68fea1b6-11b9-4737-ad79-74e49832533f
