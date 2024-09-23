d645c3de005f1604e54212c76a30650179822ba1
Christian Lechner <22294087+lechnerc77@users.noreply.github.com.>
2024-09-23 11:45:53 +0200

    docs: harmonize import examples

--- examples/resources/cloudfoundry_service_instance/import.sh
+++ examples/resources/cloudfoundry_service_instance/import.sh
@@ -1,1 +1,3 @@
+# terraform import cloudfoundry_service_instance.<resource_name> <service_instance_guid>
+
 terraform import cloudfoundry_service_instance.xsuaa_svc 68fea1b6-11b9-4737-ad79-74e49832533f
