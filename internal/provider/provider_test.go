af605c9049e2bc71c071391facb04c3bd7862f65
Debaditya Ray <65864761+Dray56@users.noreply.github.com>
2024-11-06 14:14:01 +0530

    Added service_route_binding datasource (#63)

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -325,6 +325,7 @@ func TestProvider_HasDataSources(t *testing.T) {
 		"cloudfoundry_service_broker",
 		"cloudfoundry_service_route_bindings",
 		"cloudfoundry_service_brokers",
+		"cloudfoundry_service_route_binding",
 	}
 
 	ctx := context.Background()
