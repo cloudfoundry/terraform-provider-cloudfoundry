17490a5b6fa76aa93c958a22a7d71b9341d5105c
prajin-op <prajin.op@gmail.com>
2025-03-20 12:36:45 +0530

    fix: update markdown description for delete function

--- internal/provider/resource_service_plan_visibility.go
+++ internal/provider/resource_service_plan_visibility.go
@@ -12,6 +12,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
@@ -63,6 +65,7 @@ func (r *servicePlanVisibilityResource) Schema(ctx context.Context, req resource
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.RequiresReplaceIfConfigured(),
 				},
+			},
 		},
 	}
 }
