bdbdf5d45ec32e2c70bc81e1d86b0c9194e4cf72
ANUGRAHG <anugrah.gupta@sap.com>
2026-02-25 06:00:34 +0000

    add resource identity for app resource

--- cloudfoundry/provider/resource_app.go
+++ cloudfoundry/provider/resource_app.go
@@ -24,6 +24,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/identityschema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/boolplanmodifier"
@@ -52,6 +53,10 @@ var (
 	_ resource.ResourceWithConfigure   = &appResource{}
 	_ resource.ResourceWithImportState = &appResource{}
 )
+
+type appResourceIdentityModel struct {
+	AppGUID types.String `tfsdk:"app_guid"`
+}
 
 func NewAppResource() resource.Resource {
 	return &appResource{}
@@ -340,6 +345,16 @@ func (r *appResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *
 	}
 }
 
+func (r *appResource) IdentitySchema(_ context.Context, _ resource.IdentitySchemaRequest, resp *resource.IdentitySchemaResponse) {
+	resp.IdentitySchema = identityschema.Schema{
+		Attributes: map[string]identityschema.Attribute{
+			"app_guid": identityschema.StringAttribute{
+				RequiredForImport: true,
+			},
+		},
+	}
+}
+
 func (r *appResource) ProcessSchemaAttributes() map[string]schema.Attribute {
 	pSchema := map[string]schema.Attribute{
 		"type": schema.StringAttribute{
@@ -442,7 +457,7 @@ func (r *appResource) Configure(ctx context.Context, req resource.ConfigureReque
 }
 
 func (r *appResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	r.upsert(ctx, &req.Plan, nil, &resp.State, &resp.Diagnostics)
+	r.upsert(ctx, &req.Plan, nil, &resp.State, &resp.Diagnostics, &resp.Identity)
 }
 
 func (r *appResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
@@ -487,12 +502,24 @@ func (r *appResource) Read(ctx context.Context, req resource.ReadRequest, resp *
 		plan.Stopped = appType.Stopped
 	}
 	resp.State.Set(ctx, &plan)
+
+	var identity appResourceIdentityModel
+
+	diags = req.Identity.Get(ctx, &identity)
+	if diags.HasError() {
+		identity = appResourceIdentityModel{
+			AppGUID: types.StringValue(appType.ID.ValueString()),
+		}
+
+		diags = resp.Identity.Set(ctx, identity)
+		resp.Diagnostics.Append(diags...)
+	}
 }
 
 func (r *appResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	r.upsert(ctx, &req.Plan, &req.State, &resp.State, &resp.Diagnostics)
+	r.upsert(ctx, &req.Plan, &req.State, &resp.State, &resp.Diagnostics, &resp.Identity)
 }
-func (r *appResource) upsert(ctx context.Context, reqPlan *tfsdk.Plan, reqState *tfsdk.State, respState *tfsdk.State, respDiags *diag.Diagnostics) {
+func (r *appResource) upsert(ctx context.Context, reqPlan *tfsdk.Plan, reqState *tfsdk.State, respState *tfsdk.State, respDiags *diag.Diagnostics, respIdentity **tfsdk.ResourceIdentity) {
 	var (
 		desiredState, previousState AppType
 		envs                        map[string]*string
@@ -557,6 +584,13 @@ func (r *appResource) upsert(ctx context.Context, reqPlan *tfsdk.Plan, reqState 
 	plan.CopyConfigAttributes(&desiredState)
 	plan.Stopped = desiredState.Stopped
 	respDiags.Append(respState.Set(ctx, &plan)...)
+
+	identity := appResourceIdentityModel{
+		AppGUID: types.StringValue(plan.ID.ValueString()),
+	}
+
+	diags = (*respIdentity).Set(ctx, identity)
+	respDiags.Append(diags...)
 }
 func (r *appResource) push(appType AppType, appManifestValue *cfv3operation.AppManifest, ctx context.Context) (*cfv3resource.App, error) {
 	var file *os.File
@@ -630,7 +664,11 @@ func (r *appResource) Delete(ctx context.Context, req resource.DeleteRequest, re
 }
 
 func (r *appResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
-	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+	if req.ID != "" {
+		resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+		return
+	}
+	resource.ImportStatePassthroughWithIdentity(ctx, path.Root("id"), path.Root("app_guid"), req, resp)
 }
 
 func getAppLogTrace(ctx context.Context, r *appResource, desiredState AppType, curTime time.Time) []string {
--- examples/resources/cloudfoundry_app/import.sh
+++ examples/resources/cloudfoundry_app/import.sh
@@ -1,3 +1,19 @@
 # terraform import cloudfoundry_app.<resource_name> <app_guid>
 
 terraform import 'cloudfoundry_app.gobis-server' f71f4a6e-253c-4025-8e45-d2be1a0d9b15
+
+#terraform import using id attribute in import block
+
+import {
+  to = cloudfoundry_app.<resource_name>
+  id = "<app_guid>"
+}
+
+# this resource supports import using identity attribute from Terraform version 1.12 or higher
+
+import {
+to = cloudfoundry_app.<resource_name>
+identity = {
+  app_guid = "<app_guid>"
+  }
+}
